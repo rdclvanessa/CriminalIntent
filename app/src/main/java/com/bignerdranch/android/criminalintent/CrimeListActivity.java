b8f2447b670eb259dd9e303c3283d20f99f552eb
rdclvanessa <32470478+rdclvanessa@users.noreply.github.com>
2018-02-25 12:54:17 -0600

    Chapter 8 recycler view 100

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Xander on 2/24/18.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+
+}
